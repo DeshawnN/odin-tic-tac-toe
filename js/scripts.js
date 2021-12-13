ea9e9c9de68e19c666b6ac86e312f2136e8fee59
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-13 17:48:23 -0500

    refactor: remove javascript logic to start from scratch

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
 const gameBoard = (function() {
-    const board = [];
-
-    function move(player, position) {
-        board[position] = player.getSign();
-    }
-
-    function getBoard() {
-        return board;
-    }
-
-    return { move, getBoard };
-})();
-
-const { player1, player2 } = (function() {
-    function Player(sign) {
-        function getSign() {
-            return sign;
-        }
-        
-        return { getSign };
-    }
-
-    const player1 = Player("X");
-    const player2 = Player("O");
-
-    return { player1, player2 };
+    
 })();
